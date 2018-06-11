3040194efa157d652e2234f870f0c4d099168215
Jonathan Greener <jonathan.greener@Jonathans-Mac.local>
2018-06-11 18:27:59 +0300

    added checkbox logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CheckBox from './CheckBox';
 
 class App extends Component {
   render() {
     return (
-      
+      <CheckBox>
+        <CheckBox.CheckBoxButton />
+        <CheckBox.Checked>Cb is Checked</CheckBox.Checked>
+        <CheckBox.UnChecked>Cb is UnChecked</CheckBox.UnChecked>
+      </CheckBox>
     );
   }
 }
--- src/CheckBox.js
+++ src/CheckBox.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 
+const CheckBoxContext = React.createContext({ checkedStatus: false, toggleCheckBox: () => {} });
+
 class CheckBox extends Component {
-  state = {
-    checked: false
+  componentDidMount() {
+    console.log('state is ', this.state);
+  }
+  toggleCheckBox = () => {
+    this.setState(currState => ({
+      checked: !currState.checked
+    }));
   };
 
+  state = {
+    checked: false,
+    toggleCheckBox: this.toggleCheckBox
+  };
+  static CheckBoxButton = () => {
+    return (
+      <CheckBoxContext.Consumer>
+        {cbContext => (
+          <div>
+            <input type="checkbox" value="test checkbox" onClick={cbContext.toggleCheckBox} />
+            <label>Check the box</label>
+          </div>
+        )}
+      </CheckBoxContext.Consumer>
+    );
+  };
+
+  static Checked = ({ children }) => (
+    <CheckBoxContext.Consumer>{cbContext => (cbContext.checkedStatus ? children : null)}</CheckBoxContext.Consumer>
+  );
+
+  static UnChecked = ({ children }) => (
+    <CheckBoxContext.Consumer>{cbContext => (cbContext.checkedStatus ? null : children)}</CheckBoxContext.Consumer>
+  );
+
   render() {
-    return <div />;
+    return (
+      <CheckBoxContext.Provider
+        value={{ checkedStatus: this.state.checked, toggleCheckBox: this.state.toggleCheckBox }}
+      >
+        {this.props.children}
+      </CheckBoxContext.Provider>
+    );
   }
 }
 
