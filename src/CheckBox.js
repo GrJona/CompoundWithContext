633911e1e07abea03d428a246179f3c8a463896e
Jonathan Greener <jonathan.greener@Jonathans-Mac.local>
2018-06-11 15:15:10 +0300

    first commit

---
+++ src/CheckBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CheckBox extends Component {
+  state = {
+    checked: false
+  };
+
+  render() {
+    return <div />;
+  }
+}
+
+export default CheckBox;
